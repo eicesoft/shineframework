b684c99e7067e37beeab5d008d2442f6741602d8
kelezyb <eicesoft@gmail.com>
2012-09-13 13:48:09 +0800

    语言包修改

--- Site/App/Lang/en/core.lang.php
+++ Site/App/Lang/en/core.lang.php
@@ -1,5 +1,5 @@
 <?php
 return array(
-	'error.pdo.connect' => '数据库连接失败',
-	'error.notaction' => '不存在的Action[%s:%s]',
+	'error.pdo.connect' => 'Database connection error.',
+	'error.notaction' => 'Don`t find Action[%s:%s]',
 );
