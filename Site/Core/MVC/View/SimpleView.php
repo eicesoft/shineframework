af1b845536519d540a44a28ecbb9d3965694eefa
kelezyb <eicesoft@gmail.com>
2012-09-13 18:04:50 +0800

    View

--- Site/Core/MVC/View/SimpleView.php
+++ Site/Core/MVC/View/SimpleView.php
@@ -7,16 +7,41 @@ use Core\MVC\View\View;
  * 基本页面视图
  */
 class SimpleView extends View {
-	public function __construct() {
+	private $viewDatas;
 
+	private $viewFile;
+
+	public function __construct($view) {
+		$this->viewFile = APP_PATH . DS . 'View' . $view . '.tpl.php';
 	}
 
-	public function assign() {
+	public function assign($key, $var) {
+		$this->viewDatas[$key] = $var;
+	}
 
+	public function assigns($vars) {
+		$this->viewDatas = $vars;
 	}
 
 	public function display()
 	{
+		extract($this->viewDatas);
 
+		if(is_readable($this->viewFile)) {
+			ob_start();
+			include $this->viewFile;
+			$contents = ob_get_clean();
+
+			return $contents;
+		} else {
+			if(is_scalar($this->viewDatas))
+			{
+				return $this->viewDatas;
+			}
+			else
+			{
+				return json_encode($this->viewDatas);
+			}
+		}
 	}
 }
