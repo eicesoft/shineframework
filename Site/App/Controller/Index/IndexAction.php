f798e4d594c27c9f77aaa4e46d983565822e4cb8
kelezyb <eicesoft@gmail.com>
2012-09-24 16:51:28 +0800

    Memcache

--- Site/App/Controller/Index/IndexAction.php
+++ Site/App/Controller/Index/IndexAction.php
@@ -2,16 +2,24 @@
 namespace App\Controller\Index;
 
 use Core\MVC\Controller\Action;
+use Core\MVC\Model\DataSource\MemDataSource;
 
 /**
  *
  */
 class IndexAction extends Action{
+	public function __construct() {
+	}
+
+
 	/**
 	 * @param $param
 	 * @return array
 	 */
 	public function execute($param) {
+		$mem = new MemDataSource(array('host' => '172.17.0.22', 'port' => 11211));
+		$mem->set('a1', array());
+		var_dump($mem->get('a1'));
 		return $param;
 	}
 }
